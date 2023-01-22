40380dce3f0ebb684b83550382ac43f58b29cff5
MABobrov <118075278+MABobrov@users.noreply.github.com>
2023-01-22 22:34:04 +0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -3,22 +3,19 @@ int a=1;
 int i=1;
 int b=1;
 int t=5;
+int result = 0;
 void hello() {
-	
 			while(i<10)
 			{
 				printf ("hello №%i \n ",i);
 				i=i+1;
 			}}
 			
-
 void number()
 {
-	
-	while(a<16)
+	while(a<=15)
 	{
 		printf("\n %i",a); a=a+1;
-		
 	}}
 
 void chetn(){
@@ -27,19 +24,11 @@ void chetn(){
 		if(b % 2 != 0){
 			printf ("\n    %i ",b);
 		}b=b+1;}}
-void zdn_two()
-{
-	while(t<16)
-	{
-		printf("\n    %i ",t);
-		t=t+5;
-	}
-}
-
-
-
+void zdn_two(){
+   for (int i = 5; i <= 15; i++) {
+        result += i;}
+        printf("  \n %d", result);}
 
 int main(void){ hello(), chetn(),number(),zdn_two();
-	
 	return 0;
 }
